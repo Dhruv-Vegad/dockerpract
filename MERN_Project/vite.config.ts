b6ef48bfb354ce22f66d76db2bdec2b43ea41c77
Dhruv Vegad <dhruvavegad@gmail.com>
2025-11-21 12:35:41 +0530

    Final changes commit

--- MERN_Project/vite.config.ts
+++ MERN_Project/vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
         // target: 'http://mern_project-backend-1:3001',
-        target: 'http://backend:3001',
+        //target: 'http://backend:3001',
         changeOrigin: true,
       },
     },
